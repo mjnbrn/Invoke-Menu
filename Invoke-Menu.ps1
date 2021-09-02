d503232b817682d282946ae8cd881725a092ed63
Ben <mjnbrn@gmail.com>
2021-09-02 06:28:22 -0400

    Update Invoke-Menu.ps1

--- Invoke-Menu.ps1
+++ Invoke-Menu.ps1
@@ -1,20 +1,24 @@
 function Invoke-Menu {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory = $true)]
         [array]$MenuOptions,
         [string]$MenuTitle = "Invoke-Menu: A Menu For Me"
 
     )
+    
     $OldTitle = $host.ui.RawUI.WindowTitle
     $host.ui.RawUI.WindowTitle = "$MenuTitle"
     $MenuString = ""
     0..($MenuOptions.Length - 1) | ForEach-Object {
-        if ($MenuOptions[$_] -match "^@{(.*)}$"){
-            $MenuString += "($_) $($Matches[1])"
-        }else{
-            $MenuString += "($_) $($MenuOptions[$_])"}
+        if ($MenuOptions[$_] -match "^@{(.*)}$") {
+            $MenuString += "($_) $($Matches[1])`n"
+        }
+        else {
+            $MenuString += "($_) $($MenuOptions[$_])`n"
+        }
     }
+    Write-Host $MenuString
 
     $Answer = $MenuOptions[(Read-Host "Please enter the number of your selection: ")]
     $host.ui.RawUI.WindowTitle = $OldTitle
